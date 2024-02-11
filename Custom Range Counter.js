c8c4e72115f4561cb41fa475d20d7b6b569b23f5
yankareddy <reddy.a111friend@gmail.com>
2024-02-11 19:40:09 +0530

    custom-range-counter

---
+++ Custom Range Counter.js
@@ -0,0 +1,38 @@
+let fromUserInputEl = document.getElementById("fromUserInput");
+let toUserInputEl = document.getElementById("toUserInput");
+let counterTextEl = document.getElementById("counterText");
+let startBtnEl = document.getElementById("startBtn")
+
+
+function displayNumbers(fromCount, toCount) {
+    let currentCount = fromCount;
+    counterTextEl.textContent = currentCount;
+
+
+    let timerId = setInterval(function() {
+        if (currentCount < toCount) {
+            currentCount += 1;
+            counterTextEl.textContent = currentCount;
+        } else {
+            clearInterval(timerId);
+        }
+    }, 1000);
+}
+
+
+startBtnEl.onclick = function() {
+
+    let fromVal = fromUserInputEl.value;
+    let toVal = toUserInputEl.value;
+
+    if (fromVal === "") {
+        alert("Enter the from value");
+    } else if (toVal === "") {
+        alert("Enter the to value");
+    } else {
+        let fromValInteger = parseInt(fromVal);
+        let toValInteger = parseInt(toVal);
+
+        displayNumbers(fromValInteger, toValInteger);
+    }
+}
